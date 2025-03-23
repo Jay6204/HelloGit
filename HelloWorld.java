f9b2efefbbeb2eadf908e18e0ed56eaeafe6fde0
Jay6204 <pj42215@gmail.com>
2025-03-23 11:44:05 +0530

    added slogan

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,13 @@
 // Method 1: Simple method that prints "Hello"
 class HelloWorld {
-  static void sayHello() {
-    System.out.println("Hello Jay Prakash Kumar. Welcome to the world of Java.");
-  }
+    static void sayHello() {
+        System.out.println("Hello Jay Prakash Kumar. Welcome to the world of Java.");
+    }
+
+    static void sayJaiShreeRam() {
+        System.out.println("Ek hi nara ek hi naam Jai Shree Ram Jai Shree Ram");
+    }
+  
 
   public static void main(String[] args) {
     sayHello(); // Calling the function
