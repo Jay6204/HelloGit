fbe23c8397cb2120a3eeb3b8e0810cc24ece7058
Jay6204 <pj42215@gmail.com>
2025-03-23 12:25:06 +0530

    jai bholenath

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,9 +7,15 @@ class HelloWorld {
     static void sayJaiShreeRam() {
         System.out.println("Ek hi nara ek hi naam Jai Shree Ram Jai Shree Ram");
     }
-  
+    static void sayJaiBholeShankar() {
+        System.out.println("Bhole Nath Ki Jai");
+    }
+    
 
   public static void main(String[] args) {
-    sayHello(); // Calling the function
+      sayHello();
+      sayJaiShreeRam();
+      sayJaiBholeShankar();
+    
   }
 }
