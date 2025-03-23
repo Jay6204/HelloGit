a170b4d64a30237cc424088c3b2f81adb1f390db
Jay6204 <pj42215@gmail.com>
2025-03-23 23:52:59 +0530

    temp

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,9 +7,28 @@ class HelloWorld {
     static void sayJaiShreeRam() {
         System.out.println("Ek hi nara ek hi naam Jai Shree Ram Jai Shree Ram");
     }
-  
 
+    static void sayJaiShreeKrishna() {
+        System.out.println("Jai Shree Krishna");
+    }
+    static void sayJaiBholeShankar() {
+        System.out.println("Bhole Nath Ki Jai");
+    }
+    
+
+    static void sayJaiHanuman() {
+        System.out.println("Jai Hanuman");
+    }
+
+    static void sayJaiMahakali() {
+        System.out.println("Jai Mahakali");
+    }
   public static void main(String[] args) {
-    sayHello(); // Calling the function
+      sayHello();
+      sayJaiShreeKrishna();
+      sayJaiShreeRam();
+      sayJaiHanuman();
+      sayJaiBholeShankar();
+      sayJaiMahakali();
   }
 }
